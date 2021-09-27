09ed82096a6100b721c8d1cb374093c09b1eb3c3
Nikita Gaurihar <gaurihar.n@northeastern.edu>
2021-09-27 00:07:02 -0400

    commiting assignment1

---
+++ src/model/CandidateProfile.java
@@ -0,0 +1,159 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+public class CandidateProfile {
+    
+    private String name;
+    private String geoData;
+    private String dOb;
+    private long telephnNum;
+    private long faxNum;
+    private String emailID;
+    private long ssn;
+    private long medRecordNum;
+    private long healthPlanBeneNum;
+    private long bankACNum;
+    private long certLicenNum;
+    private String vehIdentifier;
+    private String devIdentifier;
+    private String linkedIn;
+    private String ipAdress;
+    private ImageIcon image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeoData() {
+        return geoData;
+    }
+
+    public void setGeoData(String geoData) {
+        this.geoData = geoData;
+    }
+
+    public String getdOb() {
+        return dOb;
+    }
+
+    public void setdOb(String dOb) {
+        this.dOb = dOb;
+    }
+
+    public long getTelephnNum() {
+        return telephnNum;
+    }
+
+    public void setTelephnNum(long telephnNum) {
+        this.telephnNum = telephnNum;
+    }
+
+    public long getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(long faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+  
+    public long getMedRecordNum() {
+        return medRecordNum;
+    }
+
+    public void setMedRecordNum(long medRecordNum) {
+        this.medRecordNum = medRecordNum;
+    }
+
+    public long getHealthPlanBeneNum() {
+        return healthPlanBeneNum;
+    }
+
+    public void setHealthPlanBeneNum(long healthPlanBeneNum) {
+        this.healthPlanBeneNum = healthPlanBeneNum;
+    }
+
+    public long getBankACNum() {
+        return bankACNum;
+    }
+
+    public void setBankACNum(long bankACNum) {
+        this.bankACNum = bankACNum;
+    }
+
+    public long getCertLicenNum() {
+        return certLicenNum;
+    }
+
+    public void setCertLicenNum(long certLicenNum) {
+        this.certLicenNum = certLicenNum;
+    }
+
+    public String getVehIdentifier() {
+        return vehIdentifier;
+    }
+
+    public void setVehIdentifier(String vehIdentifier) {
+        this.vehIdentifier = vehIdentifier;
+    }
+
+    public String getDevIdentifier() {
+        return devIdentifier;
+    }
+
+    public void setDevIdentifier(String devIdentifier) {
+        this.devIdentifier = devIdentifier;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIpAdress() {
+        return ipAdress;
+    }
+
+    public void setIpAdress(String ipAdress) {
+        this.ipAdress = ipAdress;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+
+
+    
+}
